679f21dc1b3679684036e77569a076c26f315b1c
Márcio Lucena Aranha de Macedo <aranha1975@academicos.com>
2024-04-17 16:56:53 -0300

    inicio feita na sala2

--- Lista1_Fase2-Ex04.py
+++ Lista1_Fase2-Ex04.py
@@ -1,4 +1,6 @@
 from alfabeto import alfabeto
 alfabeto(str(input('Digite uma letra: ')))
 
+def parimpar (alfabeto.posicao):
 
+print()
--- alfabeto.py
+++ alfabeto.py
@@ -1,3 +1,6 @@
 def alfabeto(letra):
     alfabeto=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     print(alfabeto.index(letra.lower())+1)
+posicao=alfabeto.index(letra.lower())+1
+
+
